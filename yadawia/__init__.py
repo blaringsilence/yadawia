b0c4a7b60e32084eba2e73841c685f5531812ebc
Mariam Maarouf <mrf.mariam@gmail.com>
2017-04-14 20:38:46 +0200

    Bare skeleton.

--- yadawia/__init__.py
+++ yadawia/__init__.py
@@ -4,4 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-DB = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
